fb1e3d1c5faef345cb8453be4549e9d8eb3f413c
HwangHyeonseok <hhs0995@naver.com>
2025-01-16 15:42:39 +0900

    deployment

---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAonIKlQNJE1zS8mXs9iWbJBr5Bk1sL100",
+    authDomain: "sparta-f6035.firebaseapp.com",
+    projectId: "sparta-f6035",
+    storageBucket: "sparta-f6035.firebasestorage.app",
+    messagingSenderId: "379178653191",
+    appId: "1:379178653191:web:371e937b0c932e21dd3ca2",
+    measurementId: "G-LBY7RN58TE"
+};
