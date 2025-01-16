4cde899e249111921db29e2a6df1c979de5751a5
HwangHyeonseok <hhs0995@naver.com>
2025-01-16 10:50:28 +0900

    4-4 Firestore Database 시작하기

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+    // Firebase 구성 정보 설정
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAonIKlQNJE1zS8mXs9iWbJBr5Bk1sL100",
+    authDomain: "sparta-f6035.firebaseapp.com",
+    projectId: "sparta-f6035",
+    storageBucket: "sparta-f6035.appspot.com",
+    messagingSenderId: "379178653191",
+    appId: "1:379178653191:web:371e937b0c932e21dd3ca2",
+    measurementId: "G-LBY7RN58TE"
+};
